4d459f2035bf4583ba075f6383386b93cdf0ccd1
Dylan De Faoite <dylanseandefaoite@gmail.com>
2026-01-11 17:21:09 +0000

    update main.py to launch flask app

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from connectors.reddit_connector import RedditConnector
-from datetime import datetime
+import server.app
 
 if __name__ == "__main__":
-    connector = RedditConnector()
-
-    search_results = connector.search_subreddit("China", "all", limit=5, timeframe="month")
-    for post in search_results:
-        print(f"Title: {post.title}\nAuthor: {post.author}\nSubreddit: {post.subreddit}\nUpvotes: {post.upvotes}\nSource: {post.source}\nTimestamp: {datetime.fromtimestamp(post.timestamp)}")
-        print("---")
+    server.app.app.run(debug=True)
