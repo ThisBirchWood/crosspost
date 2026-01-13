b0e079599a8887a977bf6d3392e5f1b7b2923d9b
Dylan De Faoite <dylanseandefaoite@gmail.com>
2026-01-13 19:06:00 +0000

    Rename fetch data script & add check for empty posts

--- create_dataset.py
+++ create_dataset.py
@@ -1,11 +1,16 @@
 import json
 from connectors.reddit_connector import RedditConnector
 
-data_file = 'reddit_posts.json'
+data_file = 'data/reddit_posts.json'
 reddit_connector = RedditConnector()
+
+def remove_empty_posts(posts):
+    return [post for post in posts if post.content.strip() != ""]
 
 def main():
     posts = reddit_connector.get_new_subreddit_posts('cork', limit=1000)
+    posts = remove_empty_posts(posts)
+
     print(f"Fetched {len(posts)} posts from r/cork")
 
     with open(data_file, 'w') as f:
