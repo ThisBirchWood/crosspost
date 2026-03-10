0866dda8b3fe23312c9efc9d03998765b880eddd
Dylan De Faoite <dylanseandefaoite@gmail.com>
2026-03-10 18:25:05 +0000

    chore: add util to always split evenly

--- server/utils.py
+++ server/utils.py
@@ -48,3 +48,7 @@ def get_request_filters() -> dict:
         filters["data_sources"] = data_sources
 
     return filters
+
+def split_limit(limit: int, n: int) -> list[int]:
+    base, remainder = divmod(limit, n)
+    return [base + (1 if i < remainder else 0) for i in range(n)]
